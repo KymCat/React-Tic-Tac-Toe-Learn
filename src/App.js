f26f486f1b815c9f8ba78f1e8c08960c67f4cb80
KymCat <kohina1452@gmail.com>
2025-08-26 17:49:43 +0900

    보드칸 클릭시 X표시 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { useState } from "react";
+
 /*
   JSX = javaScript + XML
   컴포넌트 = JSX를 반환하는 함수
   컴포넌트를 활용하면 재사용성과 유지보수 용이
 */
-function Square() {
-  return <button className="square">1</button>;
+function Square({value, onSquareClick}) {
+  
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice(); // .slice() 로 완전히 새로배열 할당
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     /*
       리액트는 단일 JSX 엘리멘트를 반환 해야하기 때문에
@@ -16,19 +31,19 @@ export default function Board() {
 
     <>
       <div className="board-row">
-        <Square /> 
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   )
