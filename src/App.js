8405452e2f9996fd337f4c8237d9cadc7c040a36
KymCat <kohina1452@gmail.com>
2025-08-27 14:36:19 +0900

    승리 로직 추가

--- src/App.js
+++ src/App.js
@@ -20,7 +20,11 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (squares[i]) return; // 사각형이 채워져있으면 종료
+    // 사각형이 채워져있거나, 이겼으면 종료
+    if (squares[i] || calculateWinner(squares)) 
+      return;
+
+    // .slice() 로 배열 복사 -> 원본과 달라져야 렌더링을 해야기에
     const nextSquares = squares.slice();
 
     if (xIsNext) {
@@ -39,6 +43,13 @@ export default function Board() {
     */
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner)
+    status = "Winner: " + winner;
+  else 
+    status = "Next player: " + (xIsNext ? "X" : "O");
+
   return (
     /*
       리액트는 단일 JSX 엘리멘트를 반환 해야하기 때문에
@@ -46,6 +57,7 @@ export default function Board() {
     */
 
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -63,4 +75,27 @@ export default function Board() {
       </div>
     </>
   )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]; // 이기는 경우 모두 정리
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  
+  }
+
+  return null;
 }
